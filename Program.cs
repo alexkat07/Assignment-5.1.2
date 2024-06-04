790d0fec3e3c0719aea0905eb96dff00ef99de0a
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-04 10:44:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateSum(1234));
+
+            int CalculateSum(int input) 
+            {
+                int sum = 0;    
+
+                string convertToString = input.ToString();
+
+                for (int i = 0; i  < convertToString.Length; i++) 
+                {
+                    int num = int.Parse(convertToString[i].ToString());
+                    sum += num; 
+                }
+                return sum;
+            }
+        }
+    }
+}
